84b0205e93261c3052941bffa31e4bd6410d6e5a
Hassan Mostafa <hassanmostafa545@gmail.com>
2019-07-21 18:20:43 +0200

    Add Note

--- Pinterest/Theme/Theme.swift
+++ Pinterest/Theme/Theme.swift
@@ -10,7 +10,7 @@ import UIKit
 class Theme {
   //Noteworthy-Light
   //AvenirNext-Medium
-  static let AppID = 2
+  static let AppID = 2 // change this number to 1 to get Normal Grid UI
   static let mainFont = "AvenirNext-Medium"
   static let mainColor = UIColor.black
   static let secondaryColor = UIColor.white
